ed68cb068bb4f8eb0981d9f6544e02af4ec386df
clemencep <clemencepatayo@gmail.com>
2021-02-14 11:30:06 +0300

    Init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=microservicedb
+export POSTGRES_HOST=microservicedbnew.c32tbb6kcke9.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=microservicedb
+export AWS_BUCKET=patayobacket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=ClemenceP
+export JWT_SECRET=
+export URL=http://localhost:8100
